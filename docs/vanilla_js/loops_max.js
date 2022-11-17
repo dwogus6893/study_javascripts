183dd20b5926e44660791d5389114932d24addfd
dwogus6893 <dwogus6893@naver.com>
2022-11-17 09:09:24 +0900

    ss

--- docs/vanilla_js/loops_max.js
+++ docs/vanilla_js/loops_max.js
@@ -1,48 +1,52 @@
-/*
-//문제
-9개의 서로 다른 자연수가 주어질 때, 이들 중 최댓값을 찾고 그 최댓값이 몇 번째 수인지를 구하는 프로그램을 작성하시오.
-예를 들어, 서로 다른 9개의 자연수
-3, 29, 38, 12, 57, 74, 40, 85, 61
-이 주어지면, 이들 중 최댓값은 85이고, 이 값은 8번째 수이다.
-//입력
-첫째 줄부터 아홉 번째 줄까지 한 줄에 하나의 자연수가 주어진다. 주어지는 자연수는 100 보다 작다.
+// 문제
+// 9개의 서로 다른 자연수가 주어질 때, 이들 중 최댓값을 찾고 그 최댓값이 몇 번째 수인지를 구하는 프로그램을 작성하시오.
+// 예를 들어, 서로 다른 9개의 자연수
+// 3, 29, 38, 12, 57, 74, 40, 85, 61
+// 이 주어지면, 이들 중 최댓값은 85이고, 이 값은 8번째 수이다.
 
-//출력
-첫째 줄에 최댓값을 출력하고, 둘째 줄에 최댓값이 몇 번째 수인지를 출력한다.
-*/
+// 입력
+// 첫째 줄부터 아홉 번째 줄까지 한 줄에 하나의 자연수가 주어진다. 주어지는 자연수는 100 보다 작다.
 
-//공통사항(이부분 복사)
-const fs = require('fs'); //파일을 읽어오기 위해 사용
+// 출력
+// 첫째 줄에 최댓값을 출력하고, 둘째 줄에 최댓값이 몇 번째 수인지를 출력한다.
+
+//공동부분 
+const fs = require('fs');
 const filepath =
 process.platform === "linux" ? "/dev/stdin" : "docs/vanilla_js/input.txt";
-let input = fs.readFileSync(filepath).toString().trim().split("\n");
-//여기까지
+let inputs = fs.readFileSync(filepath).toString().trim().split("\n").map(Number);
+// let max = 0;
+// let index = 0;
 
-let max = input[0]; //최대값
-let i; //몇 번째 수인지 구하는 프로그램
+//-------------------------function 미적용
+// for(let i =0 ; i<inputs.length ; i++) { //inputs 길이만큼 반복
+//     if(max < inputs[i]) {       //Max가 inputs보다 작다면
+//         max = inputs[i];        //그 inputs 값이 max
+//         index = i;              //index에 그 i값 저장
+//     }
+// }
+// console.log()
 
-for(let i=1; i<input.length;i++){
-    if(max < input[i]){
-        max = input[i];
-        indexof = i;
+// console.log(`${max}\n${index+1}`); 
+
+//-------------------------for of 문 적용
+// var iterable = [3, 29, 38, 12, 57, 74, 40, 85, 61];
+
+// for(let value of iterable){
+//     console.log(value);
+// }    
+
+let max_num = inputs[0];
+let index_num = 0;
+for (let i = 0; i< inputs.length; i++){
+    if(max_num <= inputs[i])    {
+        max_num = inputs[i];
+    index_num = i+1;
     }
 }
-console.log(max);
-console.log(indexof+1);
+console.log(max_num);
+console.log(index_num);
 
 
 
 
-
-
-// let min_max = { min: 1000000, max: -1000000 };
-// for (let i = 0; i < inputs.length; i++) {
-//   if (min_max.min > inputs[i]) {
-//     min_max.min = inputs[i];
-//   }
-//   if (min_max.max < inputs[i]) {
-//     min_max.max = inputs[i];
-//   }
-// }
-
-// console.log(`${min_max.min} ${min_max.max}`);
